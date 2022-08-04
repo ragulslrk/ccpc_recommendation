f695d1b345f2ef9c73b9595271d53aeedbf57e63
Gokulprasath2002 <gokulprasath100702@gmail.com>
2022-08-04 10:26:09 +0530

    2nd

--- firebase_connect.py
+++ firebase_connect.py
@@ -2,5 +2,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-credentials = credentials.Certificate("./credentials/private_key.json")
-firebase_admin.initialize_app(credentials)
+credi = credentials.Certificate("./credentials/private_key.json")
+firebase_admin.initialize_app(credi)
